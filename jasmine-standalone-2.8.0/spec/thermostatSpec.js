ac5ce8e362706a8170e2d98e6db73bdc93fd4eb2
stephengeller <stephen.geller@hotmail.com>
2017-08-31 16:21:25 +0100

    Completed thermostat base infrastructure

--- jasmine-standalone-2.8.0/spec/thermostatSpec.js
+++ jasmine-standalone-2.8.0/spec/thermostatSpec.js
@@ -1,42 +1,75 @@
 beforeEach(function() {
   thermostat = new Thermostat();
-})
+});
 
 describe('Thermostat', function() {
   describe('#initialize', function() {
     it('temperature starts at 20 by default', function() {
       expect(thermostat.temp).toEqual(20);
     })
-  })
+  });
 
   describe('#up', function() {
     it('increases the temperature by 1', function() {
       thermostat.up();
       expect(thermostat.temp).toEqual(21);
     })
-  })
+  });
 
   describe('#down', function() {
     it('decreases the temperature by 1', function() {
       thermostat.down();
       expect(thermostat.temp).toEqual(19);
-    })
+    });
 
     it('cannot go lower then minimum temperature', function() {
-      thermostat.temp = 10
+      thermostat.temp = 10;
       thermostat.down();
       expect(thermostat.temp).toEqual(10);
     })
-  })
+  });
 
   describe('#powerMode', function() {
     it('power mode is on by default', function() {
       expect(thermostat.powerMode).toEqual(true);
-    })
+    });
+
     it('when on maximum temperature is 25', function() {
-      thermostat.temp = 25;
-      thermostat.up()
-      expect(thermostat.temp).toEqual(25);
+        thermostat.temp = 25;
+        thermostat.up();
+        expect(thermostat.temp).toEqual(25);
+    });
+
+    it('when off, maximum temperature is 32', function() {
+        thermostat.powerMode = false;
+        thermostat.temp = 32;
+        thermostat.up();
+        expect(thermostat.temp).toEqual(32);
     })
-  })
-})
+  });
+
+    describe('#reset', function() {
+      it ('resets to default temp', function() {
+          thermostat.temp = 23;
+          thermostat.reset();
+          expect(thermostat.temp).toEqual(20);
+      });
+    });
+
+    describe('#currentUsage', function() {
+      it('gives low-usage below 18 degrees', function() {
+        thermostat.temp = 15;
+          expect(thermostat.currentUsage()).toEqual('low-usage')
+      });
+
+      it('gives medium-usage below 25 degrees', function() {
+          thermostat.temp = 20;
+          expect(thermostat.currentUsage()).toEqual('medium-usage')
+      });
+
+      it('gives medium-usage below 25 degrees', function() {
+          thermostat.temp = 30;
+          expect(thermostat.currentUsage()).toEqual('high-usage')
+      });
+    });
+});
--- jasmine-standalone-2.8.0/src/thermostat.js
+++ jasmine-standalone-2.8.0/src/thermostat.js
@@ -1,20 +1,43 @@
+const POWER_MODE_MAX = 25;
+const NORMAL_MODE_MAX = 32;
+const DEFAULT_TEMP = 20;
+const MIN_TEMP = 10;
+
 var Thermostat = function() {
-  this.temp = 20;
+  this.temp = DEFAULT_TEMP;
   this.powerMode = true;
+  this.normalMaxTemp = NORMAL_MODE_MAX;
+  this.powerMaxemp = POWER_MODE_MAX;
 }
 
 Thermostat.prototype.up = function(increment = 1) {
-  if (this.temp == 25 && this.powerMode == true) {
-    this.temp = 25;
+  if ((this.temp + increment >= POWER_MODE_MAX) && this.powerMode === true) {
+      this.temp = POWER_MODE_MAX;
+  } else if(this.temp + increment >= NORMAL_MODE_MAX && this.powerMode === false) {
+      this.temp = NORMAL_MODE_MAX
   } else {
     this.temp += increment;
   }
-}
+};
 
 Thermostat.prototype.down = function(increment = 1) {
-  if (this.temp - increment <= 10) {
-    this.temp = 10;
+  if (this.temp - increment <= MIN_TEMP) {
+    this.temp = MIN_TEMP;
   } else {
     this.temp -= increment;
   }
-}
+};
+
+Thermostat.prototype.reset = function() {
+  this.temp = DEFAULT_TEMP;
+};
+
+Thermostat.prototype.currentUsage = function() {
+  if (this.temp < 18) {
+    return 'low-usage';
+  }else if (this.temp < 25) {
+    return 'medium-usage';
+  }else {
+    return 'high-usage';
+  }
+};
